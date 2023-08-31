5abdb696bf71160e253ecb701dd915db16ede2ce
hpthakur <75304242+hpthakur@users.noreply.github.com>
2023-08-31 12:23:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+// hidden/show navbar
+
+const navbar=document.getElementsByTagName("header")[0];
+ lastscroll=window.scrollY;
+
+document.addEventListener("scroll", function () {
+    if (window.scrollY>lastscroll) {
+        navbar.classList.add("hidden-head");
+        
+    } else {
+        
+        navbar.classList.remove("hidden-head");
+    }
+    lastscroll=window.scrollY;
+})
+
+
+// transparent navbar when on top
+document.addEventListener("scroll", function (){
+    if (window.scrollY===0) {
+        navbar.classList.add("transparent-head");
+    }
+    else{
+        navbar.classList.remove("transparent-head");
+
+    }
+
+})
+
+// scroll down button 
+
+document.getElementsByClassName("scroll-down")[0].addEventListener("click", function () {
+    window.scrollBy(0,window.innerHeight);
+    
+    
+})
+
+var MobileNav=document.getElementsByClassName("nav")[0];
+if (MobileNav.style.position==='absolute') {
+    // document.getElementsByTagName("body")[0].style.overflow='hidden';
+    alert("working");
+    
+}
+
+
+
+
+// mobile menu 
+
+let toggleButton=document.getElementsByClassName("fa-bars")[0];
+let crossButton=document.getElementsByClassName("fa-xmark")[0];
+
+toggleButton.addEventListener("click",function(){
+    MobileNav.classList.add("active");
+    crossButton.style.transform="translateX(0)";
+    toggleButton.style.display= "none" ;
+    document.getElementsByTagName("body")[0].style.position="fixed";
+        
+    
+        
+
+
+})
+
+crossButton.addEventListener("click",function(){
+    crossButton.style.transform="translateX(800%)";
+    MobileNav.classList.remove("active");
+    toggleButton.style.display= "block" ;
+    document.getElementsByTagName("body")[0].style.position="relative";
+})
+
